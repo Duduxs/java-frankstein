8508330cfe3108784aaa10b2375ccd099ead02f6
Eduardo J <duduxss3@gmail.com>
2024-08-21 19:43:28 -0300

    :recycle: (main) Melhora o código do 1 exercicio

--- data-structures/src/main/java/basics/recursions/exercises/ExerciseOne.java
+++ data-structures/src/main/java/basics/recursions/exercises/ExerciseOne.java
@@ -2,8 +2,6 @@ package main.java.basics.recursions.exercises;
 
 public class ExerciseOne {
 
-    static int sum = 0;
-
     private static boolean isPrime(int number, int mod) {
         if (mod <= 1) return true;
         if (number % mod == 0) return false;
@@ -15,20 +13,21 @@ public class ExerciseOne {
     }
 
     public static int sumOfPrimeNumbersBy(int interval) {
-        if (interval == 1) {
-            return -1;
+        if (interval <= 1) {
+            return 0;
         }
-        if (isPrime(interval)) sum += interval;
-
-        sumOfPrimeNumbersBy(--interval);
-        return sum;
+        if (isPrime(interval)) {
+            return interval + sumOfPrimeNumbersBy(--interval);
+        }
+        return sumOfPrimeNumbersBy(--interval);
     }
 
     public static void main(String[] args) {
 
 //        System.out.println(primeNumberOf(10));
-        System.out.println(sumOfPrimeNumbersBy(2));
-        System.out.println(sumOfPrimeNumbersBy(2));
+        System.out.println(sumOfPrimeNumbersBy(53));
+        System.out.println(sumOfPrimeNumbersBy(3));
+//        System.out.println(sumOfPrimeNumbersBy(2));
 //        IntStream.range(1,15).forEachOrdered(n -> System.out.println(n + " " +isPrime(n)));
     }
 
