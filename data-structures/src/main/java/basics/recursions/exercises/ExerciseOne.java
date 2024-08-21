d443c7d4ba35e56b2ac1897c284f91d6c79677d6
Eduardo J <duduxss3@gmail.com>
2024-08-21 20:42:52 -0300

    :sparkles: (main) Adiciona multi de fib

--- data-structures/src/main/java/basics/recursions/exercises/ExerciseOne.java
+++ data-structures/src/main/java/basics/recursions/exercises/ExerciseOne.java
@@ -22,13 +22,32 @@ public class ExerciseOne {
         return sumOfPrimeNumbersBy(--interval);
     }
 
+    private static int multiFib(int number) {
+        return multiFib(number, 1, 0, 1);
+    }
+
+    private static int multiFib(int n, int currentIndex, int previous, int current) {
+        if (currentIndex > n) return 1;
+
+        int next = previous + current;
+
+        int product = (current % 2 == 0) ? current : 1;
+
+        return product * multiFib(n, currentIndex + 1, current, next);
+    }
+
+
     public static void main(String[] args) {
 
 //        System.out.println(primeNumberOf(10));
         System.out.println(sumOfPrimeNumbersBy(53));
         System.out.println(sumOfPrimeNumbersBy(3));
+        System.out.println("----------- 2 parte ---------------");
+        System.out.println(multiFib(6));
 //        System.out.println(sumOfPrimeNumbersBy(2));
-//        IntStream.range(1,15).forEachOrdered(n -> System.out.println(n + " " +isPrime(n)));
+//        IntStream.range(1,30).forEachOrdered(n -> System.out.println(n + " " +isPrime(n)));
+//        IntStream.range(0,30).forEachOrdered(n -> System.out.println(multiFib(n)));
+
     }
 
 }
