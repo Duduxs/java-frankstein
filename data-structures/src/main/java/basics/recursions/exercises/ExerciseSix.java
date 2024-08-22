4d64b2a308a59bbbd0c7e7bb0ee1bf448d59886c
Eduardo J <duduxss3@gmail.com>
2024-08-22 09:50:21 -0300

    :recycle: (main) Ajusta exercicio para ficar mais coeso

--- data-structures/src/main/java/basics/recursions/exercises/ExerciseSix.java
+++ data-structures/src/main/java/basics/recursions/exercises/ExerciseSix.java
@@ -3,20 +3,20 @@ package main.java.basics.recursions.exercises;
 public class ExerciseSix {
 
     public static String calculateArithmeticProgression(int terms, double actual, double reason) {
-        if(terms <= 0) return "NO PA";
         return calculateArithmeticProgression(terms, 1, new StringBuilder(), actual, reason);
     }
 
     private static String calculateArithmeticProgression(int terms, int actualTerms, StringBuilder data, double actual, double reason) {
-        if(actualTerms > terms) return data.toString();
-        var s =" >> " + (actual + (actualTerms - 1) * reason);
-        data.append(s);
+        if (actualTerms > terms) return data.toString();
+        data.append(" >> ").append(actual + (actualTerms - 1) * reason);
         return calculateArithmeticProgression(terms, ++actualTerms, data, actual, reason);
     }
 
 
     public static void main(String[] args) {
-        System.out.println(calculateArithmeticProgression(5, 0.80, 0.58));
+        System.out.println(calculateArithmeticProgression(5, 0.80, 0.58)); //>> 0.8 >> 1.38 >> 1.96 >> 2.54 >> 3.12
+        System.out.println(calculateArithmeticProgression(2, 0.80, 0.58)); // >> 0.80 >> 1.38
+        System.out.println(calculateArithmeticProgression(0, 0.80, 0.58)); // empty
     }
 
 }
